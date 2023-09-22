ce56d9a7aaf08b03f7341b8dff2ee6432d466844
s29914 <>
2023-09-22 19:34:45 +0530

    0.0.2

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ uploaded_file = st.file_uploader("Upload CSV file for the Customer", type=["csv"
 
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
+    total_current_amount = df['Current'].sum()
     past_due_ranges = [col for col in df.columns if 'Past Due' in col]
     frequency_counter = df[past_due_ranges].apply(lambda x: x.gt(0)).sum()
     most_frequent_range = frequency_counter.idxmax()
@@ -13,9 +14,17 @@ if uploaded_file is not None:
     probabilities = frequency_counter / total_count
     max_probability_value = probabilities.max()
     st.write("From the given dataset probable month in which the user will pay is: ")
+    data = {'month_range': past_due_ranges, 'current amount': [0] * len(past_due_ranges)}
+    df_output = pd.DataFrame(data)
+    df_output.loc[df_output['month_range'] == most_frequent_range, 'current amount'] = total_current_amount
+    print(df_output)
+    # Streamlit app
+    st.title("Past Due Ranges")
+    # Display the DataFrame
+    st.write(df_output)
     st.write(most_frequent_range)
-    st.write("Probability of the Past Due Month is:")
-    st.write(f"{max_probability_value * 100:.2f} %")
+    # st.write("Probability of the Past Due Month is:")
+    # st.write(f"{max_probability_value * 100:.2f} %")
 
 else:
     st.info("Please upload a CSV file for a customer")
